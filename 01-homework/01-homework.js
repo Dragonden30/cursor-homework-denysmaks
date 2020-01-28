64071b11b9ab415156c1594440a691e88316479e
Denys <denking30@gmail.com>
2020-01-28 19:20:38 +0200

    Small change 2 to my homework 1

--- 01-homework/01-homework.js
+++ 01-homework/01-homework.js
@@ -4,6 +4,6 @@ let PriceHigh = 123.965;
 
 let PriceMid = 90.2345;
 
-let PriceMax = Math.max(PriceLow,PriceMax,PriceMid)
+let PriceMax = Math.max(PriceLow,PriceHigh,PriceMid)
 
 console.log("MaxNum "+ PriceMax)
