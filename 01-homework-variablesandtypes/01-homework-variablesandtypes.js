1219d39c061287a6239fc355553608580c17a5d1
Denys <denking30@gmail.com>
2020-02-03 20:20:17 +0200

    Fixing errors

--- 01-homework-variablesandtypes/01-homework-variablesandtypes.js
+++ 01-homework-variablesandtypes/01-homework-variablesandtypes.js
@@ -1,36 +1,36 @@
-let priceLow = 15.678;
+const priceLow = 15.678;
 
-let priceHigh = 123.965;
+const priceHigh = 123.965;
 
-let priceMid = 90.2345;
+const priceMid = 90.2345;
 
-let priceMax = Math.max(priceLow,priceHigh,priceMid);
+const priceMax = Math.max(priceLow,priceHigh,priceMid);
 
 console.log("Maximum number: "+ priceMax);
 
-let priceMin = Math.min(priceLow,priceHigh,priceMid);
+const priceMin = Math.min(priceLow,priceHigh,priceMid);
 
 console.log("Minimum number: "+ priceMin);
 
-let sumOfPrices = priceLow + priceMid + priceHigh;
+const sumOfPrices = priceLow + priceMid + priceHigh;
 
 console.log("Sum of products:  "+ sumOfPrices);
 
-let priceLowFloored = Math.floor(priceLow);
+const priceLowFloored = Math.floor(priceLow);
 
-let priceHighFloored = Math.floor(priceHigh);
+const priceHighFloored = Math.floor(priceHigh);
 
-let priceMidFloored = Math.floor(priceMid);
+const priceMidFloored = Math.floor(priceMid);
 
-let sumOfFlooredPrices = priceLowFloored + priceHighFloored + priceMidFloored;
+const sumOfFlooredPrices = priceLowFloored + priceHighFloored + priceMidFloored;
 
 console.log("Sum of products 'floored':  "+ sumOfFlooredPrices);
 
-let priceCeil = Math.ceil((priceLow + priceHigh + priceMid)/100)*100;
+const priceCeil = Math.ceil((priceLow + priceHigh + priceMid)/100)*100;
 
 console.log("Sum of products 'ceiled to 100':  "+ priceCeil);
 
-let sumOfPricesFlooded = Math.floor(sumOfPrices) 
+const sumOfPricesFlooded = Math.floor(sumOfPrices) 
 
 const sumEvenOrNot = sumOfPricesFlooded % 2 > 0 ? "Even" : "Odd";
 
@@ -38,23 +38,23 @@ console.log("Flooded sum of products is "+ sumEvenOrNot);
 
 const customerMoney = 500;
 
-let chargeFromSum = customerMoney - sumOfPrices;
+const chargeFromSum = customerMoney - sumOfPrices;
 
 console.log("Charge from " + customerMoney + "will be "+ shargeFromSum);
 
-let midPrice = sumOfPrices/3;
+const midPrice = sumOfPrices/3;
 
 console.log("Midle price of " + midPrice + " with round to 2 number after dot will be "+ midPrice.toFixed(2));
 
-let randSaleNum = (Math.random(100)).toFixed(2);
+const randSaleNum = (Math.random(100)).toFixed(2);
 
 console.log("The sale: " + randSaleNum*100 +"%");
 
-let anotherSumOfPrices = (priceHigh + priceLow).toFixed(2);
+const anotherSumOfPrices = (priceHigh + priceLow).toFixed(2);
 
-let salePrice = (anotherSumOfPrices * randSaleNum).toFixed(2);
+const salePrice = (anotherSumOfPrices * randSaleNum).toFixed(2);
 
-let lostOfPrice = anotherSumOfPrices/2 - salePrice;
+const lostOfPrice = anotherSumOfPrices/2 - salePrice;
 
 console.log("The lost of price will be: " + anotherSumOfPrices/2 + " - " + salePrice + " -> " + lostOfPrice.toFixed(2));
 
