1b3ec8da10d159d1b8c932f39b0f3bd1e10c24cd
Denys <denking30@gmail.com>
2020-02-03 17:26:17 +0200

    Added everything else

--- 02-homework-loopsandcondtions/02-homework-loopsandcondtions.js
+++ 02-homework-loopsandcondtions/02-homework-loopsandcondtions.js
@@ -2,14 +2,73 @@ let charecterN = prompt(`Enter First Number: `,`0`);
 
 let numberN = parseInt(charecterN);
 
+console.log(numberN);
+
 console.log(typeof numberN);
 
 let charecterM = prompt(`Enter Second Number: `,`0`);
 
 let numberM = parseInt(charecterM);
 
+console.log(numberM);
+
 console.log(typeof numberM);
 
 let hardChoice = confirm(`Do you want to kill all Even numbers?`);
 
-console.log(hardChoice)
+console.log(hardChoice);
+
+let numberSet = new Set(); // добавил по приколу чтоб проверять себя
+
+let operatNum = 0;
+
+if (hardChoice){
+
+    while (numberN <= numberM){
+
+        if((numberN % 2) != 0){
+
+            numberSet.add(numberN);
+
+            operatNum = operatNum + numberN;
+
+            numberN++;
+
+            continue;
+
+        }else{
+
+        numberN++;
+    
+    }
+    
+        console.log(numberSet);
+
+        console.log(numberN);
+
+        console.log(operatNum);
+    }
+
+    alert(operatNum);
+
+}else{
+
+    while (numberN <= numberM){
+
+        numberSet.add(numberN);
+                    
+        operatNum = operatNum + numberN;
+
+        numberN++;
+    
+        console.log(numberSet);
+
+        console.log(numberN);
+
+        console.log(operatNum);
+
+    }
+
+    alert(operatNum);
+
+}
