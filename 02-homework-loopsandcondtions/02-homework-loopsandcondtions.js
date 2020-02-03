12e5a6994139e66bf164cfde1af453f83ea5edef
Denys <denking30@gmail.com>
2020-02-03 16:13:36 +0200

    Added confirm

--- 02-homework-loopsandcondtions/02-homework-loopsandcondtions.js
+++ 02-homework-loopsandcondtions/02-homework-loopsandcondtions.js
@@ -1,11 +1,15 @@
-let charecterN = prompt('Enter First Number: ','0');
+let charecterN = prompt(`Enter First Number: `,`0`);
 
 let numberN = parseInt(charecterN);
 
 console.log(typeof numberN);
 
-let charecterM = prompt('Enter Second Number: ','0');
+let charecterM = prompt(`Enter Second Number: `,`0`);
 
 let numberM = parseInt(charecterM);
 
 console.log(typeof numberM);
+
+let hardChoice = confirm(`Do you want to kill all Even numbers?`);
+
+console.log(hardChoice)
