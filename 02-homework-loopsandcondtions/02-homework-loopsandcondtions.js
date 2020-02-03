3a2dcbde85f1613ccf490fe06ef8e0d2a3736e2f
Denys <denking30@gmail.com>
2020-02-03 15:43:42 +0200

    Added prompt of number N and M

---
+++ 02-homework-loopsandcondtions/02-homework-loopsandcondtions.js
@@ -0,0 +1,11 @@
+let charecterN = prompt('Enter First Number: ','0');
+
+let numberN = parseInt(charecterN);
+
+typeof numberN;
+
+let charecterM = prompt('Enter Second Number: ','0');
+
+let numberM = parseInt(charecterM);
+
+typeof numberM;
