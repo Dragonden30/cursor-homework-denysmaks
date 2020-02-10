02437b8cf59dd0ce9de0ab18646afc32c4eada85
Denys <denking30@gmail.com>
2020-02-10 16:05:02 +0200

    Homework 3 lots of added exept pamanama(i cant remember) and dublikates

--- 03-homework-functions/03-homework-functions.js
+++ 03-homework-functions/03-homework-functions.js
@@ -35,4 +35,126 @@ let userNameFixed = (userName) => {
 
 }
 
-userNameFixed(`DaViD`);
+let userBillPayCalc = (userSalary) => {
+
+    let billPrice = userSalary * 0.2;
+
+    return console.log(`Result of userBillPayCalc =` + (userSalary - billPrice) + ' .');
+
+}
+
+let getRandomInt = (min, max) => {
+
+    min = Math.ceil(min);
+
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min)) + min; 
+  
+}
+
+let userLetterCount = (letter, word) => {
+
+ let counter = 0;
+
+ word = word.toLowerCase();
+ 
+ letter = letter.toLowerCase();
+
+for (place = 0; place < word.length; place++) {
+
+    if (word.charAt(place) == letter) {
+
+        counter += 1;
+
+    }
+
+}
+
+  return console.log(`Result of userLetterCount = ` + counter + `;`);
+
+}
+
+let convertCurrency = (currency) => {
+
+    let convertedCurrency = 0;
+ 
+    if (currency.substr(-1) == `$`) {
+    
+        currencySliced = Number(currency.slice(0, currency.length-1));
+
+        convertedCurrency = currencySliced * 24;
+
+        return console.log(`Result of convertCurrency = ` + convertedCurrency + ` .`);
+    
+    }
+    
+    if (currency.substr(-3) == `UAH`) {
+
+        currencySliced = Number(currency.slice(0, currency.length-3));
+
+        convertedCurrency = currencySliced / 24;
+
+        return console.log(`Result of convertCurrency = ` + convertedCurrency + ` .`);
+
+    }
+
+    else{
+
+        return console.log(`undefiend`);
+        
+    }
+
+}
+
+let getRandPass = (length) => {
+
+    let randNumber = 0;
+
+    if(length){
+
+    randNumber = Math.floor(Math.random() * (10**length));
+
+    }
+    else{
+
+        randNumber = Math.floor(Math.random() * (10**8));
+
+    }
+    return console.log(`Result of getRandPass = ` + randNumber);
+
+}
+
+let deleteLetters = (letter,word) => {
+
+    let counter = 0;
+
+    let wordReplaced = 0;
+
+    word = word.toLowerCase();
+ 
+    letter = letter.toLowerCase();
+    
+    for (place = 0; place < word.length; place++) {
+
+        if (word.charAt(place) == letter) {
+    
+            counter += 1;
+    
+        }
+    
+    }
+
+    wordReplaced = word.replace(letter, ``);
+
+    for (cycle = 0; cycle < counter; cycle++){
+
+        wordReplaced = wordReplaced.replace(letter, ``);
+        
+    }
+
+    return console.log(`Reult of deleteLetters = ` + wordReplaced + `;`);
+
+}
+
+deleteLetters(`a`,`blablabla`);
