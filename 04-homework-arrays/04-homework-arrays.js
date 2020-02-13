d624e7c4862b3201840c2a64fae63da5b0f8d9d1
Denys <denking30@gmail.com>
2020-02-13 23:24:03 +0200

    Homework 4 finished for now

---
+++ 04-homework-arrays/04-homework-arrays.js
@@ -0,0 +1,74 @@
+const students = ["Саша", "Игорь", "Лена", "Ира", "Алексей", "Светлана"];
+
+const themes = ["Дифференциальные уравнения", "Теория автоматов", "Алгоритмы и структуры данных"];
+
+const marks = [4, 5, 5, 3, 4, 5];
+
+const getPairs = (students) =>{
+
+    
+    return [[students[0],students[2]],[students[1],students[3]],[students[4],students[5]]];
+
+}
+
+const manNotMan = getPairs(students);
+
+const getPairsAndThemes = (pairs, themes) => {
+
+    let joinedPairs = [];
+
+    for (let someInt = 0; someInt < pairs.length;  someInt++){
+
+        joinedPairs.push([pairs[someInt].join(" и "), themes[someInt]]);
+
+    }
+
+    return joinedPairs;
+    
+}
+
+const getTheme = getPairsAndThemes(manNotMan, themes);
+
+const getMark = (students,marks) =>{
+
+    let marksAndStudents = [];
+
+    for(let someInt = 0; someInt < students.length; someInt++){
+
+        marksAndStudents.push([students[someInt],marks[someInt]]);
+
+    }
+
+    return marksAndStudents;
+
+}
+
+const marksNSt = getMark(students,marks);
+
+const getRandMark = (complexAr) =>{
+
+    const min = 1;
+
+    const max = 5;
+    
+    let markRandomizer = Math.floor(Math.random() * (max - min + 1) + min);
+    
+    let randMarks = [];
+
+    for(let someInt = 0; someInt < complexAr.length; someInt++){
+
+        randMarks.push([complexAr[someInt], markRandomizer]);
+    }
+
+    return randMarks;
+}
+
+const randMarks = getRandMark(getTheme);
+
+console.log(manNotMan);
+
+console.log(getTheme);
+
+console.log(marksNSt);
+
+console.log(randMarks);
