9a27c436721e664b4a59f4ca1448875d0b7e138e
Denys <denking30@gmail.com>
2020-02-27 02:05:52 +0200

    Homework 7 edited

--- 07-homework-objectthis/07-homework-objectthis.js
+++ 07-homework-objectthis/07-homework-objectthis.js
@@ -2,6 +2,38 @@ const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
 const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
-function getMyTaxes(country, salary){
-    
+function getMyTaxes(salary){
+    return this.tax * salary;
 }
+
+console.log("getMyTaxes function result :");
+console.log(getMyTaxes.call(ukraine, 2000));
+
+function getMiddleTaxes(country){
+    return this.tax * this.middleSalary;
+}
+
+console.log("getMiddleTaxes function results :");
+console.log(getMiddleTaxes.call(ukraine));
+
+function getTotalTaxes(country){
+    return this.tax * this.middleSalary * this.vacancies;
+}
+
+console.log("getTotalTaxes function results :");
+console.log(getTotalTaxes.call(ukraine));
+
+function getTotalTaxes(country){
+    setInterval(() => {
+        const min = 1500;
+        const max = 2000;
+        const number = Math.floor(Math.random() * (max - min + 1) + min);
+        console.log({
+            number: number,
+            taxes: Math.floor(this.tax * number),
+            profit: number- Math.floor(this.tax * number)
+        });
+    }, 10000);
+}
+
+getTotalTaxes.call(ukraine)
