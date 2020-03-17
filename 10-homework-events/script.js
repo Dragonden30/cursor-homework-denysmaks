89f14f6667427459eb9cdcdfaca742f5dad9e22b
Denys <denking30@gmail.com>
2020-03-17 18:03:28 +0200

    Homework 10 Finished

---
+++ 10-homework-events/script.js
@@ -0,0 +1,24 @@
+function sounds(key) {
+	const soundId =  document.querySelector(`[data-key="${key}"]`);
+	const soundName = soundId.id;
+	if (soundId) {
+		const sound = new Audio(`sounds/`+ soundName +`.mp3`);
+		sound.play();
+		soundId.style.backgroundColor = 'yellow'
+        setTimeout(function () {
+			soundId.style.backgroundColor = '#A0522D'
+		}, 400)
+	}
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(element => {
+    element.addEventListener("click", function () {
+		sounds(this.innerText);
+	});
+});
+
+document.addEventListener('keydown', function (event) {
+	const keyPushed = event.key;
+	sounds(keyPushed);
+});
